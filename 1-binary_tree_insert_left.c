058d27844fcdaa8fba78e3cbfd91d53e36e33464
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-03 06:23:33 +1000

    insert

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     /* Check if parent is NULL */
 	if (parent == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
 
 	if (parent->left == NULL)
